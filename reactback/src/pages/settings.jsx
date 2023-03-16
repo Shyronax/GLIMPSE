2b13675a437ddb02c2e6b90282bd08161ae6b91e
Erelae <s.enriquez@outlook.fr>
2023-03-16 15:37:41 +0100

    update pgaes

--- reactback/src/pages/settings.jsx
+++ reactback/src/pages/settings.jsx
@@ -1,1 +1,8 @@
+import React from 'react';
 import { NavLink, NavItem } from '../components/nav'
+
+export const Settings = () => {
+    return(
+        <h1>Settings page</h1>
+    )
+}
--- reactback/src/pages/stats.jsx
+++ reactback/src/pages/stats.jsx
@@ -1,1 +1,8 @@
+import React from 'react';
 import { NavLink, NavItem } from '../components/nav'
+
+export const Stats = () => {
+    return(
+        <h1>Satistics page</h1>
+    )
+}
