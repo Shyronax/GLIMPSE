a37ba05a862c492cfe8065a426bf82754bbbaca0
Erelae <s.enriquez@outlook.fr>
2023-03-16 15:31:48 +0100

    update routing

---
+++ reactback/src/pages/accounts.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { NavLink, NavItem } from '../components/nav';
+
+const Accounts = () => {
+    return(
+        <h1>accounts page</h1>
+    )
+}
+
+export default Accounts;
