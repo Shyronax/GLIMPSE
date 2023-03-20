cb73dce9be0cced5480cac728a97b3705ee02dde
xshuky <itsjuneforwork@gmail.com>
2023-03-20 23:44:11 +0100

    add first structure

--- view/view-mentions.php
+++ view/view-mentions.php
@@ -12,36 +12,54 @@
     ?>
     <main>
         <div class="container">
-            <h1 class="container__title">Bienvenue Ponito !</h1>
+            <h1 class="container__title">Mentions légales</h1>
             <div class="section">
                 <h2 class="section__title">Sommaire</h2>
                 <ul class="section__list">
-                    <li class="section__list-item"><a href="" class="section__list-item-link">Édition</a></li>
-                    <li class="section__list-item"><a href="" class="section__list-item-link">Conception - Habillage du site</a></li>
-                    <li class="section__list-item"><a href="" class="section__list-item-link">Hébergement</a></li>
-                    <li class="section__list-item"><a href="">DPO - Collecte de données</a></li>
-                    <li class="section__list-item"><a href="" class="section__list-item-link">Politique de gestion des données personnelles</a></li>
-                    <li class="section__list-item"><a href="" class="section__list-item-link">Conditions Générales d''Utilisation (CGU)</a></li>
-                    <li class="section__list-item"><a href="" class="section__list-item-link">Contenu intégré d'autres sites web</a></li>
-                    <li class="section__list-item"><a href="" class="section__list-item-link">Autorisation du droit à l'image pour les vidéos et images</a></li>
-                    <li class="section__list-item"><a href="" class="section__list-item-link">Sources des médias (images & vidéos)</a></li>
+                    <li class="section__list-item"><a href="#edition" class="section__list-item-link">Édition</a></li>
+                    <li class="section__list-item"><a href="#conception" class="section__list-item-link">Conception - Habillage du site</a></li>
+                    <li class="section__list-item"><a href="#hebergement" class="section__list-item-link">Hébergement</a></li>
+                    <li class="section__list-item"><a href="#dpo" class="section__list-item-link">DPO - Collecte de données</a></li>
+                    <li class="section__list-item"><a href="#data" class="section__list-item-link">Politique de gestion des données personnelles</a></li>
+                    <li class="section__list-item"><a href="#cgu" class="section__list-item-link">Conditions Générales d''Utilisation (CGU)</a></li>
+                    <span id="edition"></span>
+                    <li class="section__list-item"><a href="#autre" class="section__list-item-link">Contenu intégré d'autres sites web</a></li>
+                    <li class="section__list-item"><a href="#autorisation" class="section__list-item-link">Autorisation du droit à l'image pour les vidéos et images</a></li>
+                    <li class="section__list-item"><a href="#media" class="section__list-item-link">Sources des médias (images & vidéos)</a></li>
                 </ul>
             </div>
             <div class="section">
-                <h2 class="section__title">Édition  </h2>
-                <p class="section__text">Département Métiers du Multimédia et de l'Internet de l'IUT de Champs-sur-Marne <br>
-                2, rue Albert Einstein <br>
-                77420 Champs-sur-Marne, France </p>
-                <a href="tel:0160958585" class="section__link">01 60 95 85 85</a>
-                <a href="mailto:mmi-dir.iut@univ-eiffel.fr" class="section__link">mmi-dir.iut@univ-eiffel.fr</a>
+                <h2 class="section__title">Édition</h2>
+                <div class="section__container-text">
+                    <span id="conception"></span>
+                    <p class="section__text">Département Métiers du Multimédia et de l'Internet de l'IUT de Champs-sur-Marne <br>
+                    2, rue Albert Einstein <br>
+                    77420 Champs-sur-Marne, France </p>
+                    <a href="tel:0160958585" class="section__link">01 60 95 85 85</a> <br>
+                    <a href="mailto:mmi-dir.iut@univ-eiffel.fr" class="section__link">mmi-dir.iut@univ-eiffel.fr</a>
+                </div>
             </div>
             <div class="section">
                 <h2 class="section__title">Conception - Habillage du site</h2>
-                <p class="section__text">Glimpse <br>
-                2, rue Albert Einstein <br>
-                77420 Champs-sur-Marne, France </p>
-                <a href="tel:0160958585" class="section__link">01 60 95 85 85</a>
-                <a href="mailto:glimpse@yey.fr" class="section__link">glimpse@yey.fr</a>
+                <div class="section__container-text">
+                    <span id="hebergement"></span>
+                    <p class="section__text">Glimpse <br>
+                    2, rue Albert Einstein <br>
+                    77420 Champs-sur-Marne, France </p>
+                    <a href="tel:0160958585" class="section__link">01 60 95 85 85</a> <br>
+                    <a href="mailto:glimpse@yey.fr" class="section__link">glimpse@yey.fr</a>
+                </div>
+            </div>
+            <div class="section">
+                <h2 class="section__title">Hébergement</h2>
+                <div class="section__container-text">
+                    <p class="section__text">Hostinger <br>
+                    Hostinger International Ltd (Hostinger International Ltd. private limited company) <br>
+                    61, rue Lordou Vironos <br>
+                    6023 Larnaca, Chypre (”Hostinger”) </p>
+                    <a href="mailto:compliance@hostinger.com" class="section__link">compliance@hostinger.com</a> <br>
+                    <a href="https://www.hostinger.fr/" class="section__link">hostinger.fr</a>
+                </div>
             </div>
         </div>
     </main>
