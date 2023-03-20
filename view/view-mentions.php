064ea47588035d14a0465695e0217405dcdae192
xshuky <itsjuneforwork@gmail.com>
2023-03-20 23:04:26 +0100

    add structure

---
+++ view/view-mentions.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <?php 
+        include "src/element/head.html";
+    ?>
+    <title>Mon compte - Mille Cultures, Une Origine</title>
+</head>
+<body>
+    <?php 
+        include "src/element/nav.html";
+    ?>
+    <main>
+        <div class="container">
+            <h1 class="container__title">Bienvenue Ponito !</h1>
+            <div class="section">
+                <h2 class="section__title">Sommaire</h2>
+                <ul class="section__list">
+                    <li class="section__list-item"><a href="" class="section__list-item-link">Édition</a></li>
+                    <li class="section__list-item"><a href="" class="section__list-item-link">Conception - Habillage du site</a></li>
+                    <li class="section__list-item"><a href="" class="section__list-item-link">Hébergement</a></li>
+                    <li class="section__list-item"><a href="">DPO - Collecte de données</a></li>
+                    <li class="section__list-item"><a href="" class="section__list-item-link">Politique de gestion des données personnelles</a></li>
+                    <li class="section__list-item"><a href="" class="section__list-item-link">Conditions Générales d''Utilisation (CGU)</a></li>
+                    <li class="section__list-item"><a href="" class="section__list-item-link">Contenu intégré d'autres sites web</a></li>
+                    <li class="section__list-item"><a href="" class="section__list-item-link">Autorisation du droit à l'image pour les vidéos et images</a></li>
+                    <li class="section__list-item"><a href="" class="section__list-item-link">Sources des médias (images & vidéos)</a></li>
+                </ul>
+            </div>
+            <div class="section">
+                <h2 class="section__title">Édition  </h2>
+                <p class="section__text">Département Métiers du Multimédia et de l'Internet de l'IUT de Champs-sur-Marne <br>
+                2, rue Albert Einstein <br>
+                77420 Champs-sur-Marne, France </p>
+                <a href="tel:0160958585" class="section__link">01 60 95 85 85</a>
+                <a href="mailto:mmi-dir.iut@univ-eiffel.fr" class="section__link">mmi-dir.iut@univ-eiffel.fr</a>
+            </div>
+            <div class="section">
+                <h2 class="section__title">Conception - Habillage du site</h2>
+                <p class="section__text">Glimpse <br>
+                2, rue Albert Einstein <br>
+                77420 Champs-sur-Marne, France </p>
+                <a href="tel:0160958585" class="section__link">01 60 95 85 85</a>
+                <a href="mailto:glimpse@yey.fr" class="section__link">glimpse@yey.fr</a>
+            </div>
+        </div>
+    </main>
+    <?php 
+        include "src/element/footer.html";
+    ?>
+</body>
+</html> 
