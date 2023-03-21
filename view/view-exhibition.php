bca122fc80226152079e62f85fd8dc47d7ab9ee8
xshuky <itsjuneforwork@gmail.com>
2023-03-21 12:59:22 +0100

    add text for section and flex--start

--- view/view-exhibition.php
+++ view/view-exhibition.php
@@ -17,10 +17,10 @@
         <div class="main__header">
             <h1 class="main__title">L'exposition</h1>
         </div>
-        <div class="container">
+        <div class="container flex--start">
             <div class="main-container">
                 <div class="section">
-                    <p class="section__text text--bold">Épisode de la série « Mille Cultures, une Origine », l’exposition “Les Pueblos” a pour objectif de <span class="text--highlight">démystifier les origines amérindiennes</span>.</p>
+                    <p class="section__text text--bold">Épisode de la série « Mille Cultures, une Origine », l'exposition “Les Pueblos” a pour objectif de <span class="text--highlight">démystifier les origines amérindiennes</span>.</p>
                     <p class="section__text">Cette exposition est une présentation de l'histoire fascinante, de la culture et de la vie quotidienne des peuples Pueblos amérindiens qui habitent les villages traditionnels du Sud-Ouest des États-Unis.</p>
                     <video src=""></video>
                     <p class="section__text">Vous serez plongé dans une immersion complète de l'univers des Pueblos, mettant en lumière leur riche patrimoine culturel, leur artisanat renommé, leur architecture unique, leur agriculture durable et leur spiritualité profonde. Des artefacts et des objets d'art exceptionnels vous permettront d'explorer leurs traditions artistiques, notamment leurs célèbres poteries, tissages et bijoux.</p>
@@ -38,7 +38,7 @@
                     <div class="section__header">
                         <h2 class="section__title">Dispositif interactif</h2>
                     </div>
-                    <p class="section__text text--bold"></p>
+                    <p class="section__text text--bold">Au moyen d'une tablette tactile, et guidés par nos mascottes Ponito et Kohana, vous aurez l'occasion durant l'exposition de dessiner des symboles de la culture Pueblos et de découvrir ce qu'ils représentent.</p>
                     <p class="section__text"></p>
                     <video src=""></video>
                 </div>
