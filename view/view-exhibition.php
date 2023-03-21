82cfe61de3075dc85fb4832ff9af711ae3a2d4f8
xshuky <itsjuneforwork@gmail.com>
2023-03-21 12:00:31 +0100

    add structure

---
+++ view/view-exhibition.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <?php 
+        include "src/element/head.html";
+    ?>
+    <title>Mon compte - Mille Cultures, Une Origine</title>
+</head>
+<body>
+    <?php 
+        include "src/element/nav.html";
+    ?>
+    <main class="main">
+        <div class="main__banner">
+            <img src="" alt="" class="main__banner-img">
+        </div>
+        <div class="main__header">
+            <h1 class="main__title">L'exposition</h1>
+        </div>
+        <div class="container">
+            <div class="main-container">
+                <div class="section">
+                    <p class="section__text text--bold">Épisode de la série « Mille Cultures, une Origine », l’exposition “Les Pueblos” a pour objectif de <span class="text--highlight">démystifier les origines amérindiennes</span>.</p>
+                    <p class="section__text">Cette exposition est une présentation de l'histoire fascinante, de la culture et de la vie quotidienne des peuples Pueblos amérindiens qui habitent les villages traditionnels du Sud-Ouest des États-Unis.</p>
+                    <video src=""></video>
+                    <p class="section__text">Vous serez plongé dans une immersion complète de l'univers des Pueblos, mettant en lumière leur riche patrimoine culturel, leur artisanat renommé, leur architecture unique, leur agriculture durable et leur spiritualité profonde. Des artefacts et des objets d'art exceptionnels vous permettront d'explorer leurs traditions artistiques, notamment leurs célèbres poteries, tissages et bijoux.</p>
+                    <p class="section__text">Nous espérons que cette exposition vous offrira un aperçu authentique de la culture et de l'histoire des Pueblos, et qu'elle vous inspirera à en apprendre davantage sur ces communautés !</p>
+                </div>
+                <div class="section">
+                    <div class="section__header">
+                        <h2 class="section__title">Plan du parcours</h2>
+                    </div>
+                    <p class="section__text text--bold"></p>
+                    <p class="section__text"></p>
+                    <img src="" alt="">
+                </div>
+                <div class="section">
+                    <div class="section__header">
+                        <h2 class="section__title">Dispositif interactif</h2>
+                    </div>
+                    <p class="section__text text--bold"></p>
+                    <p class="section__text"></p>
+                    <video src=""></video>
+                </div>
+            </div>
+        </div>
+    </main>
+    <?php 
+        include "src/element/footer.html";
+    ?>
+</body>
+</html> 
