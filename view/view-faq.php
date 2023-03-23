1559e20dd04c3b36b626c208db54946c62e8333a
xshuky <itsjuneforwork@gmail.com>
2023-03-23 22:34:52 +0100

    add structure

---
+++ view/view-faq.php
@@ -0,0 +1,175 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <?php 
+        include "src/element/head.html";
+    ?>
+    <title>FAQ - Mille Cultures, Une Origine</title>
+</head>
+<body>
+    <?php 
+        include "src/element/nav.html";
+    ?>
+    <main>
+        <div class="container">
+            <h1 class="container__title">Foire aux questions (FAQ)</h1>
+            <div class="section">
+                <h2 class="section__title">Horaires et accès</h2>
+                <ul class="section__list">
+                    <li class="section__list-item"><a href="#edition" class="section__list-item-link">Édition</a></li>
+                    <li class="section__list-item"><a href="#conception" class="section__list-item-link">Conception - Habillage du site</a></li>
+                    <li class="section__list-item"><a href="#hebergement" class="section__list-item-link">Hébergement</a></li>
+                    <li class="section__list-item"><a href="#dpo" class="section__list-item-link">DPO - Collecte de données</a></li>
+                    <li class="section__list-item"><a href="#data" class="section__list-item-link">Politique de gestion des données personnelles</a></li>
+                    <li class="section__list-item"><a href="#cgu" class="section__list-item-link">Conditions Générales d''Utilisation (CGU)</a></li>
+                    <span id="edition"></span>
+                    <li class="section__list-item"><a href="#autre" class="section__list-item-link">Contenu intégré d'autres sites web</a></li>
+                    <li class="section__list-item"><a href="#autorisation" class="section__list-item-link">Autorisation du droit à l'image pour les vidéos et images</a></li>
+                    <li class="section__list-item"><a href="#media" class="section__list-item-link">Sources des médias (images & vidéos)</a></li>
+                </ul>
+            </div>
+            <div class="section">
+                <h2 class="section__title">Édition</h2>
+                <div class="section__container-text">
+                    <span id="conception"></span>
+                    <p class="section__text">Département Métiers du Multimédia et de l'Internet de l'IUT de Champs-sur-Marne <br>
+                    2, rue Albert Einstein <br>
+                    77420 Champs-sur-Marne, France </p>
+                    <a href="tel:0160958585" class="section__link">01 60 95 85 85</a> <br>
+                    <a href="mailto:mmi-dir.iut@univ-eiffel.fr" class="section__link">mmi-dir.iut@univ-eiffel.fr</a>
+                </div>
+            </div>
+            <div class="section">
+                <h2 class="section__title">Conception - Habillage du site</h2>
+                <div class="section__container-text">
+                    <span id="hebergement"></span>
+                    <p class="section__text">Glimpse <br>
+                    2, rue Albert Einstein <br>
+                    77420 Champs-sur-Marne, France </p>
+                    <a href="tel:0160958585" class="section__link">01 60 95 85 85</a> <br>
+                    <a href="mailto:glimpse@yey.fr" class="section__link">glimpse@yey.fr</a>
+                </div>
+            </div>
+            <div class="section">
+                <h2 class="section__title">Hébergement</h2>
+                <div class="section__container-text">
+                <span id="dpo"></span>
+                    <p class="section__text">Hostinger <br>
+                    Hostinger International Ltd (Hostinger International Ltd. private limited company) <br>
+                    61, rue Lordou Vironos <br>
+                    6023 Larnaca, Chypre (”Hostinger”) </p>
+                    <a href="mailto:compliance@hostinger.com" class="section__link">compliance@hostinger.com</a> <br>
+                    <a href="https://www.hostinger.fr/" class="section__link">hostinger.fr</a>
+                </div>
+            </div>
+            <div class="section">
+                <h2 class="section__title">DPO - Collecte de données</h2>
+                <div class="section__container-text">
+                    <span id="data"></span>
+                    <p class="section__text">Le DPO (Data Protection Officer) peut exercer sur les données recueillies le droit de modification, d'accès, de suppression, d'oubli ainsi que de portabilité. </p>
+                    <p>Les données confiées sont strictement utilisées dans le cadre du fonctionnement de notre site. Pour toute question, contacter le DPO : <br>
+                    Valérie LAPEYRE <br>
+                    2, rue Albert Einstein <br>
+                    77420 Champs-sur-Marne, France </p>
+                    <a href="mailto:valerie.lapeyre@edu.univ-eiffel.fr" class="section__link">valerie.lapeyre@edu.univ-eiffel.fr</a> <br>
+                </div>
+            </div>
+            <div class="section">
+                <h2 class="section__title">Billets et tarifs</h2>
+                <div class="sub-section">
+                    <h3 class="sub-section__title">Quels sont les prix d’un billet ?</h3>
+                    <p class="sub-section__text">Nous pouvons collecter des données personnelles vous concernant lorsque vous utilisez notre site web. Cela peut inclure des informations telles que votre nom, votre prénom, votre âge, votre adresse email et vos informations bancaires que vous nous fournissez lors de la réservation ou la création de compte sur le site.</p>
+                </div>
+                <div class="sub-section">
+                    <h3 class="sub-section__title">Comment bénéficier d’un tarif réduit ?</h3>
+                    <p class="sub-section__text">Les données personnelles collectées par le site sont uniquement utilisées pour réaliser les tâches nécessaires au bon fonctionnement de ce dernier. Ces données ne sont pas vendues, échangées ou louées à des tiers. </p>
+                </div>
+                <div class="sub-section">
+                    <h3 class="sub-section__title">Puis-je me faire rembourser mon billet ?</h3>
+                    <p class="sub-section__text">Nous stockons vos données personnelles aussi longtemps que nécessaire pour remplir les objectifs pour lesquels elles ont été collectées.</p>
+                </div>
+                <div class="sub-section">
+                    <h3 class="sub-section__title">Quels sont les moyens de paiement autorisés ?</h3>
+                    <p class="sub-section__text">Nous ne partageons et ne vendons pas vos données personnelles à des tiers. Nous pouvons partager vos données personnelles avec des prestataires de services tiers nécessaires au bon fonctionnement de ce site web comme son hébergeur. Nous exigeons de ces tiers qu'ils respectent la vie privée et la sécurité de vos données personnelles.</p>
+                </div>
+                <div class="sub-section">
+                    <h3 class="sub-section__title">Dois-je imprimer mon billet ?</h3>
+                    <p class="sub-section__text">Vous avez le droit de demander l'accès, la rectification, la suppression et la portabilité de vos données personnelles. Vous avez également le droit de vous opposer au traitement de vos données personnelles pour des raisons légitimes. Si vous avez des questions ou des préoccupations concernant la gestion de vos données personnelles, veuillez contacter le DPO : <a href="mailto:valerie.lapeyre@edu.univ-eiffel.fr" class="section__link">valerie.lapeyre@edu.univ-eiffel.fr</a>.</p>
+                </div>
+            </div>
+            <div class="section">
+                <h2 class="section__title">Pendant la visite</h2>
+                <div class="sub-section">
+                    <h3 class="sub-section__title">Ai-je le droit de prendre des photos ?</h3>
+                    <p class="sub-section__text">Le site de réservation de tickets pour le musée [nom du musée] permet aux utilisateurs de réserver des tickets pour les expositions du musée en ligne. Le site peut également offrir des fonctionnalités supplémentaires pour les utilisateurs inscrits.</p>
+                </div>
+                <div class="sub-section">
+                    <h3 class="sub-section__title">Puis-je venir avec une valise ?</h3>
+                    <p class="sub-section__text">La création d'un compte utilisateur est facultative et n'est pas nécessaire pour effectuer une réservation. Cependant, si vous choisissez de créer un compte utilisateur, vous devez fournir un login non-utilisé par un autre utilisateur, une adresse e-mail valide et un mot de passe sécurisé. Vous êtes entièrement responsable de la confidentialité de votre mot de passe, de votre compte et ses données. Vous êtes également entièrement responsable de toutes les activités qui se produisent sous votre compte. </p>
+                </div>
+                <div class="sub-section">
+                    <h3 class="sub-section__title">Puis-je déposer un sac pendant ma visite ?</h3>
+                    <p class="sub-section__text">Pour réserver des tickets pour une exposition, vous devez sélectionner la date l'heure de visite, ainsi que le nombre de tickets par catégories (plein tarifs, moins de 26 ans, enfants de moins de 10 ans). Vous devez ensuite remplir une adresse email de réservation avec confirmation de l'adresse (remplies automatiquement dans le cas d'une connexion à un compte).</p>
+                </div>
+                <div class="sub-section">
+                    <h3 class="sub-section__title">Combien de temps faut-il pour visiter l’exposition ?</h3>
+                    <p class="sub-section__text">La vente de billets pour un musée ou de prestations liées à celui-ci est assimilée à une vente de services de loisirs et, conformément aux articles L 221-28° et L 121-21-8-12° du Code de la consommation, n'ouvre aucun droit au délai de rétractation. <span class="text--bold">Ainsi, les billets ne sont échangeables, ni modifiables, ni remboursables dans le cas où nos établissements sont ouverts au public.</span>
+                </div>
+                <div class="sub-section">
+                    <h3 class="sub-section__title">Est-ce possible de sortir et entrer plusieurs fois dans la même journée avec mon billet ?</h3>
+                    <p class="sub-section__text">L'ensemble des médias et contenus présents sur ce site web et créés en interne par ses concepteurs et son éditeur, tels que les textes, les images, les vidéos, les sons, les logos ou graphiques, ainsi que leur mise en page, leur organisation et leur présentation, sont la propriété exclusive de l'éditeur ou de ses partenaires, et sont protégés par les lois relatives à la propriété intellectuelle en vigueur.</p>
+                </div>
+                <div class="sub-section">
+                    <h3 class="sub-section__title">Ai-je le droit de dessiner dans les salles ?</h3>
+                    <p class="sub-section__text">L'IUT de Marne-la-Vallée ne sera en aucun cas responsable de tout dommage, y compris les dommages indirects, consécutifs ou spéciaux, découlant de l'utilisation ou de l'impossibilité d'utiliser le site de réservation de tickets pour les expositions “Mille Cultures, une Origine”. L'IUT de Marne-la-Vallée ne garantit pas que le site sera disponible sans interruption ou sans erreur.</p>
+                </div>
+                <div class="sub-section">
+                    <h3 class="sub-section__title">Les animaux sont-ils acceptés ?</h3>
+                    <p class="sub-section__text">L'IUT de Marne-la-Vallée se réserve le droit de modifier ces CGU à tout moment et sans préavis. Les modifications entreront en vigueur dès leur publication sur le site. Il est recommandé aux utilisateurs de consulter régulièrement ces CGU pour prendre connaissance de toute mise à jour.</p>
+                </div>
+            </div>
+            <div class="section">
+                <h2 class="section__title">Enfants et famille</h2>
+                <div class="sub-section">
+                    <h3 class="sub-section__title">Faut-il réserver pour les enfants ?</h3>
+                    <p class="sub-section__text">Le site de réservation de tickets pour le musée [nom du musée] permet aux utilisateurs de réserver des tickets pour les expositions du musée en ligne. Le site peut également offrir des fonctionnalités supplémentaires pour les utilisateurs inscrits.</p>
+                </div>
+                <div class="sub-section">
+                    <h3 class="sub-section__title">Il y a-t-il des activités pour les enfants ?</h3>
+                    <p class="sub-section__text">La création d'un compte utilisateur est facultative et n'est pas nécessaire pour effectuer une réservation. Cependant, si vous choisissez de créer un compte utilisateur, vous devez fournir un login non-utilisé par un autre utilisateur, une adresse e-mail valide et un mot de passe sécurisé. Vous êtes entièrement responsable de la confidentialité de votre mot de passe, de votre compte et ses données. Vous êtes également entièrement responsable de toutes les activités qui se produisent sous votre compte. </p>
+                </div>
+                <div class="sub-section">
+                    <h3 class="sub-section__title">Les poussettes sont-elles acceptées dans l’établissement ?</h3>
+                    <p class="sub-section__text">Pour réserver des tickets pour une exposition, vous devez sélectionner la date l'heure de visite, ainsi que le nombre de tickets par catégories (plein tarifs, moins de 26 ans, enfants de moins de 10 ans). Vous devez ensuite remplir une adresse email de réservation avec confirmation de l'adresse (remplies automatiquement dans le cas d'une connexion à un compte).</p>
+                </div>
+            </div>
+            <div class="section">
+                <h2 class="section__title">Accompagner son groupe</h2>
+                <div class="sub-section">
+                    <h3 class="sub-section__title">Puis-je réserver pour une trentaine de personnes ?</h3>
+                    <p class="sub-section__text">Le site de réservation de tickets pour le musée [nom du musée] permet aux utilisateurs de réserver des tickets pour les expositions du musée en ligne. Le site peut également offrir des fonctionnalités supplémentaires pour les utilisateurs inscrits.</p>
+                </div>
+                <div class="sub-section">
+                    <h3 class="sub-section__title">Pouvons-nous faire une pause ou déjeuner sur place avec mon groupe ?</h3>
+                    <p class="sub-section__text">La création d'un compte utilisateur est facultative et n'est pas nécessaire pour effectuer une réservation. Cependant, si vous choisissez de créer un compte utilisateur, vous devez fournir un login non-utilisé par un autre utilisateur, une adresse e-mail valide et un mot de passe sécurisé. Vous êtes entièrement responsable de la confidentialité de votre mot de passe, de votre compte et ses données. Vous êtes également entièrement responsable de toutes les activités qui se produisent sous votre compte. </p>
+                </div>
+                <div class="sub-section">
+                    <h3 class="sub-section__title">Mon groupe comprend une personne en situation de handicap. Où et comment puis-je être aidé ?</h3>
+                    <p class="sub-section__text">Pour réserver des tickets pour une exposition, vous devez sélectionner la date l'heure de visite, ainsi que le nombre de tickets par catégories (plein tarifs, moins de 26 ans, enfants de moins de 10 ans). Vous devez ensuite remplir une adresse email de réservation avec confirmation de l'adresse (remplies automatiquement dans le cas d'une connexion à un compte).</p>
+                </div>
+                <div class="sub-section">
+                    <h3 class="sub-section__title">Quels sont mes droits et mes devoirs lors de la visite en tant que responsable de groupe ?</h3>
+                    <p class="sub-section__text">La vente de billets pour un musée ou de prestations liées à celui-ci est assimilée à une vente de services de loisirs et, conformément aux articles L 221-28° et L 121-21-8-12° du Code de la consommation, n'ouvre aucun droit au délai de rétractation. <span class="text--bold">Ainsi, les billets ne sont échangeables, ni modifiables, ni remboursables dans le cas où nos établissements sont ouverts au public.</span>
+                </div>
+                <div class="sub-section">
+                    <h3 class="sub-section__title">Quand puis-je réserver ? Combien de temps avant la visite dois-je réserver ?</h3>
+                    <p class="sub-section__text">L'ensemble des médias et contenus présents sur ce site web et créés en interne par ses concepteurs et son éditeur, tels que les textes, les images, les vidéos, les sons, les logos ou graphiques, ainsi que leur mise en page, leur organisation et leur présentation, sont la propriété exclusive de l'éditeur ou de ses partenaires, et sont protégés par les lois relatives à la propriété intellectuelle en vigueur.</p>
+                </div>
+            </div>
+        </div>
+    </main>
+    <?php 
+        include "src/element/footer.html";
+    ?>
+</body>
+</html> 
