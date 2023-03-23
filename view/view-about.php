3af93e7b575ad5f9fc9f9c5fdd21e4c241222bf1
xshuky <itsjuneforwork@gmail.com>
2023-03-23 15:55:23 +0100

    add structure

---
+++ view/view-about.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <?php 
+        include "src/element/head.html";
+    ?>
+    <title>Notre histoire - Mille Cultures, Une Origine</title>
+</head>
+<body>
+    <?php 
+        include "src/element/nav.html";
+    ?>
+    <main>
+        <div class="main__banner">
+            <img src="src/img/banner-visit.jpg" alt="" class="main__banner-img">
+        </div>
+        <div class="main__header">
+            <h1 class="main__title">Notre histoire</h1>
+        </div>
+        <div class="container flex--start">
+            <div class="main-container">
+                <div class="section">
+                    <div class="section__header">
+                        <h2 class="section__title">Création de l'exposition</h2>
+                    </div>
+                    <p class="section__text">L'exposition “Mille Cultures, une Origine - Les Pueblos” a débuté son parcours en janvier 2023, date à laquelle le département BUT MMI de l'IUT de Marne-la-Vallée (Université Gustave Eiffel - Champs-sur-Marne) a commandé l'exposition à l'agence glimpse, un groupe de 7 étudiants MMI de deuxième année.</p>
+
+                    <p class="section__text">Pour plus d'infos sur l'agence, visitez le site de glimpse sur glimpse.fr.</p>
+                </div>
+                <div class="section">
+                    <div class="section__header">
+                        <h2 class="section__title">L'équipe glimpse</h2>
+                    </div>
+                    <div class="section-gallery">
+                        <div class="section-gallery__row">
+                            <div class="section-gallery__col">
+                                <img src="" alt="" class="section-gallery__col-img">
+                                <p class="section-gallery__col-name">Valérie LAPEYRE</p>
+                                <p class="section-gallery__col-text">Développeuse back</p>
+                            </div>
+                            <div class="section-gallery__col">
+                                <img src="" alt="" class="section-gallery__col-img">
+                                <p class="section-gallery__col-name">Clara MANY</p>
+                                <p class="section-gallery__col-text">Cheffe de projet, développeuse front-end, & UX designer</p>
+                            </div>
+                            <div class="section-gallery__col">
+                                <img src="" alt="" class="section-gallery__col-img">
+                                <p class="section-gallery__col-name">Samuel ENRIQUEZ-SARANO</p>
+                                <p class="section-gallery__col-text">Développeur front-end & UX designer</p>
+                            </div>
+                            <div class="section-gallery__col">
+                                <img src="" alt="" class="section-gallery__col-img">
+                                <p class="section-gallery__col-name">Oscar POISSON</p>
+                                <p class="section-gallery__col-text">Développeur full-stack</p>
+                            </div>
+                        </div>
+                        <div class="section-gallery__row">
+                            <div class="section-gallery__col">
+                                <img src="" alt="" class="section-gallery__col-img">
+                                <p class="section-gallery__col-name">Yanis CHOUCHA</p>
+                                <p class="section-gallery__col-text">Graphiste, vidéaste & sound designer</p>
+                            </div>
+                            <div class="section-gallery__col">
+                                <img src="" alt="" class="section-gallery__col-img">
+                                <p class="section-gallery__col-name">Samuel MIGUEL</p>
+                                <p class="section-gallery__col-text">Graphiste, UI designer & vidéaste</p>
+                            </div>
+                            <div class="section-gallery__col">
+                                <img src="" alt="" class="section-gallery__col-img">
+                                <p class="section-gallery__col-name">Timothé BUREAU</p>
+                                <p class="section-gallery__col-text">Chef de projet, graphiste, </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+    <?php 
+        include "src/element/footer.html";
+    ?>
+</body>
+</html> 
