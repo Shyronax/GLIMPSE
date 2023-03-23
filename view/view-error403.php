48aa2ca552bcb9b16f2b6e487cc34523f45fedbf
xshuky <itsjuneforwork@gmail.com>
2023-03-23 16:52:45 +0100

    add container--row

--- view/view-error403.php
+++ view/view-error403.php
@@ -11,14 +11,12 @@
         include "src/element/nav.html";
     ?>
     <main>
-        <div class="container">
-            <div class="main-container">
-                <img src="src/img/drawing/kohana-run.png" alt="" class="container__img">
-                <div class="section">
-                    <h1 class="container__title">Error 403 - Accès interdit</h1>
-                    <p class="container__text">Oh la la ! Je suis vraiment désolée mais il semble que l'accès au contenu demandé soit interdit. <br>
-                    Vous pouvez retourner en arrière ou encore vous rendre en page d'accueil.</p>
-                </div>
+        <div class="container container--row">
+            <img src="src/img/drawing/kohana-run.png" alt="" class="container__img">
+            <div class="section">
+                <h1 class="container__title">Error 403 - Accès interdit</h1>
+                <p class="container__text">Oh la la ! Je suis vraiment désolée mais il semble que l'accès au contenu demandé soit interdit. <br>
+                Vous pouvez retourner en arrière ou encore vous rendre en page d'accueil.</p>
             </div>
         </div>
     </main>
