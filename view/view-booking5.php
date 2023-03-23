d14b578584093ca023c9605e849885a6b69dfa54
xshuky <itsjuneforwork@gmail.com>
2023-03-23 21:30:46 +0100

    add card recap

--- view/view-booking5.php
+++ view/view-booking5.php
@@ -16,8 +16,20 @@
             <div>
                 <h1 class="container__title">Confirmation de la réservation</h1>
             </div>
-            <p class="container__text text--center">Votre <span class="text--bold">réservation</span> a bien été <span class="text--bold">enregistrée</span> ! <br>
-            Votre billet vous a été envoyé à cette adresse <span class="text--bold">monemail@gmail.com</span>. <br>
+            <p class="container__text text--center">Votre <span class="text--bold">réservation</span> a bien été <span class="text--bold">enregistrée</span> ! </p>
+            <h2 class="container__subtitle">Récapitulatif de la commande</h2>
+            <div class="review-card">
+                <div class="flex">
+                    <i class="fa-solid fa-ticket"></i>
+                    <h2 class="review-card__title">Réservation</h2>
+                </div>
+                <ul class="list--none">
+                    <li><span class="text--bold">Date :</span> le 11 avril 2023 de 8h30 à 10h30</li>
+                    <li><span class="text--bold">Nombre de places :</span> 5 places</li>
+                    <i><span class="text--bold">Total :</span> 12€</i>
+                </ul>
+            </div>  
+            <p class="container__text">Votre billet vous a été envoyé à cette adresse <span class="text--bold">monemail@gmail.com</span>. <br>
             Vous pouvez également télécharger votre billet en cliquant sur le bouton ci-dessous.</p>
             <a href="" class="btn btn--secondary">Télécharger mon billet en PDF</a>   
             <a href="controller.php?page=home" class="container__link">Retourner à l'accueil</a>
