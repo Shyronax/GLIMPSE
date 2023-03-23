9f59355f6a77a0d39fa98580e9a3c8973d9fc042
xshuky <itsjuneforwork@gmail.com>
2023-03-23 21:15:17 +0100

    add button

--- view/view-mailconf.php
+++ view/view-mailconf.php
@@ -14,6 +14,7 @@
         <div class="container">
             <h1 class="container__title">Email envoyé !</h1>
             <p class="container__text">Vérifier votre boîte de réception ou vos spam, nous vous avons envoyé un email contenant le lien de réinitialisation.</p>
+            <a href="controller.php?page=home" class="btn btn--secondary">Retourner à la page d'accueil</a>
         </div>
     </main>
     <?php 
