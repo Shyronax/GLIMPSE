c9f344cdce7d82fdc13b8067b2553f3eb15409f2
v5slv <98457679+v5slv@users.noreply.github.com>
2023-03-28 11:32:04 +0200

    add phpmail request (it works !!)

--- sendmails/resetPwdRequest.php
+++ sendmails/resetPwdRequest.php
@@ -13,23 +13,33 @@ if(isset($_POST['mail'])){
     insertIntoReinitMdp($_POST['mail'], $selector, $token, $expires); // insérer le nouveau token
 
     $to=$_POST['mail'];
-    $subject="Réinitialisation de votre mot de passe";
 
-    $mail = new PHPMailer;
+    // Paramètres SMTP Hostinger
+    $mail = new PHPMailer(true);
     $mail->isSMTP();
     $mail->SMTPDebug = 2;
     $mail->Host = 'smtp.hostinger.fr';
-    $mail->Port = 465;
+    $mail->SMTPSecure = 'tls';
+    $mail->Port = 587;
     $mail->SMTPAuth = true;
     $mail->Username = 'site@milleculturesuneorigine.but-mmi-champs.fr';
     $mail->Password = 'Milleculturesuneorigine77420!';
+
+    // Configuration de l'adresse mail
     $mail->setFrom('site@milleculturesuneorigine.but-mmi-champs.fr', 'Mille Cultures, une Origine');
-    $mail->addAddress($to, $to);
+    $mail->addAddress($to);
+
+    // Configuration du sujet et le corps de l'email
     $mail->Subject = 'Réinitialisation de votre mot de passe';
-    $mail->msgHTML(file_get_contents('message.html'), __DIR__); // message.html est le fichier contenant le message du mail, dans le même répertoare que ce fichier
-    $mail->Body = 'contenu du message en clair';
- 
-    header('Location: controller.php?page=mailenvoye');'
+    // $mail->msgHTML(file_get_contents('message.html'), __DIR__); // message.html est le fichier contenant le message du mail, dans le même répertoare que ce fichier
+    $mail->Body    = "Réinitialisez de votre mot de passe en cliquant sur ce lien (valable 30 minutes) : $url";
+    
+    if(!$mail->send()) {
+        echo "Erreur : l'email n'a pas été envoyé. Contactez site@milleculturesuneorigine.but-mmi-champs.fr";
+    } else {
+        header('Location: controller.php?page=mailenvoye');
+    }
 } else {
     header("Location: controller.php?page=pwdforgot");
 }
+
