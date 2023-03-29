964a045e5e4d0eac9cbf1fe00d0151721e189a24
v5slv <98457679+v5slv@users.noreply.github.com>
2023-03-29 16:17:58 +0200

    add PDF ticket mail and modify downloadticket into createticketpdf

--- sendMailPwd.php
+++ sendMailPwd.php
@@ -33,7 +33,6 @@ if(isset($_POST['mail'])){
     $mail = new PHPMailer(true);
     $mail->CharSet="UTF-8";
     $mail->isSMTP();
-    $mail->SMTPDebug = 2;
 
     // Paramètres SMTP Hostinger
     $mail->Host = 'smtp.hostinger.fr';
