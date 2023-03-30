448224f20eded7acff7c07a10a8e906a11200607
Valerie <98457679+v5slv@users.noreply.github.com>
2023-03-30 04:34:56 +0200

    update api

--- back-office/back/api/db.php
+++ back-office/back/api/db.php
@@ -1,2 +1,2 @@
 <?php
-$db = new PDO('mysql:host=localhost;dbname=u380906830_mcuo;port=3306;charset=utf8','u380906830_mcuo', 'MCUO_bdd23!yeah');
+$db = new PDO('mysql:host=localhost;dbname=mcuo;port=3306;charset=utf8','root', '');
